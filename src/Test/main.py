8fcf75e70894b3f41604cb3c466599c192ba0672
Mohammad Shakerian <shakerian.mmd@gmail.com>
2021-11-18 18:23:31 +0330

    Checking the difference between staging and commit -a

--- src/Test/main.py
+++ src/Test/main.py
@@ -1,5 +1,14 @@
 #Learning 
 def Student():
-    def __init__(self,):
+    def __init__(self,std_id):
+        self.std_id = std_id
+        print(f'Student with student id of {self.std_id} was added')
+        
+        
         pass
+    
+    
+    
+    
+    
 
