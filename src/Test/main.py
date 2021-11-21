502fcb48dfd001e97429dde2e52eaead7b9a1179
Mohammad Shakerian <shakerian.mmd@gmail.com>
2021-11-21 16:20:24 +0330

    main.py fixed and sub.py added

--- src/Test/main.py
+++ src/Test/main.py
@@ -1,9 +1,17 @@
 #Learning
-def Student():
+class Student():
     def __init__(self,std_id):
         self.std_id = std_id
         print(f'Student with student id of {self.std_id} was added')
-        pass
-st1 = Student(81077)
+        self.courses = []
+    def add_course(self, course_name):
+        self.courses.append(course_name)
+    def __str__(self) -> str:
+        return f'Student with id {self.std_id} and courses : {self.courses}'
 
+
+        
+st1 = Student(81077)
+st1.add_course("chem")
+print(st1)
 
